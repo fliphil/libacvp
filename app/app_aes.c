908a1a36bda9508c15bc26e2e3ce490496c44890
Philip Perricone <prperri@gmail.com>
2019-03-13 14:27:58 -0400

    Move gcm stuff into it's own function

--- app/app_aes.c
+++ app/app_aes.c
@@ -28,6 +28,89 @@
 #include <openssl/evp.h>
 #include "app_lcl.h"
 
+static int gcm(ACVP_SYM_CIPHER_TC *tc) {
+    EVP_CIPHER_CTX *cipher_ctx = NULL;
+    const EVP_CIPHER *cipher;
+    unsigned char iv_fixed[4] = { 1, 2, 3, 4 };
+    int rc = 1, ret = 0;
+
+    switch (tc->key_len) {
+    case 128:
+        cipher = EVP_aes_128_gcm();
+        break;
+    case 192:
+        cipher = EVP_aes_192_gcm();
+        break;
+    case 256:
+        cipher = EVP_aes_256_gcm();
+        break;
+    default:
+        printf("Unsupported AES-GCM key length\n");
+        return 1;
+    }
+
+    /* Begin encrypt code section */
+    cipher_ctx = EVP_CIPHER_CTX_new();
+    EVP_CIPHER_CTX_init(cipher_ctx);
+
+    if (tc->direction == ACVP_SYM_CIPH_DIR_ENCRYPT) {
+        EVP_CipherInit(cipher_ctx, cipher, NULL, NULL, 1);
+        EVP_CIPHER_CTX_ctrl(cipher_ctx, EVP_CTRL_GCM_SET_IVLEN, tc->iv_len, 0);
+        EVP_CipherInit(cipher_ctx, NULL, tc->key, NULL, 1);
+
+        EVP_CIPHER_CTX_ctrl(cipher_ctx, EVP_CTRL_GCM_SET_IV_FIXED, 4, iv_fixed);
+        if (!EVP_CIPHER_CTX_ctrl(cipher_ctx, EVP_CTRL_GCM_IV_GEN, tc->iv_len, tc->iv)) {
+            printf("acvp_aes_encrypt: iv gen error\n");
+            goto end;
+        }
+        if (tc->aad_len) {
+            EVP_Cipher(cipher_ctx, NULL, tc->aad, tc->aad_len);
+        }
+        EVP_Cipher(cipher_ctx, tc->ct, tc->pt, tc->pt_len);
+        EVP_Cipher(cipher_ctx, NULL, NULL, 0);
+        EVP_CIPHER_CTX_ctrl(cipher_ctx, EVP_CTRL_GCM_GET_TAG, tc->tag_len, tc->tag);
+    } else if (tc->direction == ACVP_SYM_CIPH_DIR_DECRYPT) {
+        EVP_CipherInit_ex(cipher_ctx, cipher, NULL, tc->key, NULL, 0);
+        EVP_CIPHER_CTX_ctrl(cipher_ctx, EVP_CTRL_GCM_SET_IVLEN, tc->iv_len, 0);
+        EVP_CIPHER_CTX_ctrl(cipher_ctx, EVP_CTRL_GCM_SET_IV_FIXED, -1, tc->iv);
+        if (!EVP_CIPHER_CTX_ctrl(cipher_ctx, EVP_CTRL_GCM_IV_GEN, tc->iv_len, tc->iv)) {
+            printf("\nFailed to set IV");
+            goto end;
+        }
+        if (tc->aad_len) {
+            /*
+             * Set dummy tag before processing AAD.  Otherwise the AAD can
+             * not be processed.
+             */
+            EVP_CIPHER_CTX_ctrl(cipher_ctx, EVP_CTRL_GCM_SET_TAG, tc->tag_len, tc->tag);
+            EVP_Cipher(cipher_ctx, NULL, tc->aad, tc->aad_len);
+        }
+        /*
+         * Set the tag when decrypting
+         */
+        EVP_CIPHER_CTX_ctrl(cipher_ctx, EVP_CTRL_GCM_SET_TAG, tc->tag_len, tc->tag);
+
+        /*
+         * Decrypt the CT
+         */
+        EVP_Cipher(cipher_ctx, tc->pt, tc->ct, tc->ct_len);
+        /*
+         * Check the tag
+         */
+        ret = EVP_Cipher(cipher_ctx, NULL, NULL, 0);
+        if (ret) goto end;
+    }
+
+    /* Success */
+    rc = 0;
+
+end:
+    /* Cleanup */
+    if (cipher_ctx) EVP_CIPHER_CTX_free(cipher_ctx);
+
+    return rc;
+}
+
 /*
  * This fuction is invoked by libacvp when an AES crypto
  * operation is needed from the crypto module being
@@ -38,109 +121,32 @@
  * test case.
  */
 int app_aes_handler_aead(ACVP_TEST_CASE *test_case) {
-    ACVP_SYM_CIPHER_TC      *tc;
-    EVP_CIPHER_CTX *cipher_ctx = NULL;
-    const EVP_CIPHER        *cipher;
-    unsigned char iv_fixed[4] = { 1, 2, 3, 4 };
-    int rc = 0;
+    ACVP_SYM_CIPHER_TC *tc = NULL;
     int ret = 0;
 
     if (!test_case) {
         return 1;
     }
 
+    /* Resolve the union to the symmetric struct */
     tc = test_case->tc.symmetric;
 
-    if (tc->direction != ACVP_SYM_CIPH_DIR_ENCRYPT && tc->direction != ACVP_SYM_CIPH_DIR_DECRYPT) {
+    if (tc->direction != ACVP_SYM_CIPH_DIR_ENCRYPT
+        && tc->direction != ACVP_SYM_CIPH_DIR_DECRYPT) {
         printf("Unsupported direction\n");
         return 1;
     }
 
-    /* Begin encrypt code section */
-    cipher_ctx = EVP_CIPHER_CTX_new();
-    EVP_CIPHER_CTX_init(cipher_ctx);
-
     /* Validate key length and assign OpenSSL EVP cipher */
     switch (tc->cipher) {
     case ACVP_AES_GCM:
-        switch (tc->key_len) {
-        case 128:
-            cipher = EVP_aes_128_gcm();
-            break;
-        case 192:
-            cipher = EVP_aes_192_gcm();
-            break;
-        case 256:
-            cipher = EVP_aes_256_gcm();
-            break;
-        default:
-            printf("Unsupported AES-GCM key length\n");
-            rc = 1;
-            goto end;
-        }
-        if (tc->direction == ACVP_SYM_CIPH_DIR_ENCRYPT) {
-            EVP_CipherInit(cipher_ctx, cipher, NULL, NULL, 1);
-            EVP_CIPHER_CTX_ctrl(cipher_ctx, EVP_CTRL_GCM_SET_IVLEN, tc->iv_len, 0);
-            EVP_CipherInit(cipher_ctx, NULL, tc->key, NULL, 1);
-
-            EVP_CIPHER_CTX_ctrl(cipher_ctx, EVP_CTRL_GCM_SET_IV_FIXED, 4, iv_fixed);
-            if (!EVP_CIPHER_CTX_ctrl(cipher_ctx, EVP_CTRL_GCM_IV_GEN, tc->iv_len, tc->iv)) {
-                printf("acvp_aes_encrypt: iv gen error\n");
-                rc = 1;
-                goto end;
-            }
-            if (tc->aad_len) {
-                EVP_Cipher(cipher_ctx, NULL, tc->aad, tc->aad_len);
-            }
-            EVP_Cipher(cipher_ctx, tc->ct, tc->pt, tc->pt_len);
-            EVP_Cipher(cipher_ctx, NULL, NULL, 0);
-            EVP_CIPHER_CTX_ctrl(cipher_ctx, EVP_CTRL_GCM_GET_TAG, tc->tag_len, tc->tag);
-        } else if (tc->direction == ACVP_SYM_CIPH_DIR_DECRYPT) {
-            EVP_CipherInit_ex(cipher_ctx, cipher, NULL, tc->key, NULL, 0);
-            EVP_CIPHER_CTX_ctrl(cipher_ctx, EVP_CTRL_GCM_SET_IVLEN, tc->iv_len, 0);
-            EVP_CIPHER_CTX_ctrl(cipher_ctx, EVP_CTRL_GCM_SET_IV_FIXED, -1, tc->iv);
-            if (!EVP_CIPHER_CTX_ctrl(cipher_ctx, EVP_CTRL_GCM_IV_GEN, tc->iv_len, tc->iv)) {
-                printf("\nFailed to set IV");
-                rc = 1;
-                goto end;
-            }
-            if (tc->aad_len) {
-                /*
-                 * Set dummy tag before processing AAD.  Otherwise the AAD can
-                 * not be processed.
-                 */
-                EVP_CIPHER_CTX_ctrl(cipher_ctx, EVP_CTRL_GCM_SET_TAG, tc->tag_len, tc->tag);
-                EVP_Cipher(cipher_ctx, NULL, tc->aad, tc->aad_len);
-            }
-            /*
-             * Set the tag when decrypting
-             */
-            EVP_CIPHER_CTX_ctrl(cipher_ctx, EVP_CTRL_GCM_SET_TAG, tc->tag_len, tc->tag);
-
-            /*
-             * Decrypt the CT
-             */
-            EVP_Cipher(cipher_ctx, tc->pt, tc->ct, tc->ct_len);
-            /*
-             * Check the tag
-             */
-            ret = EVP_Cipher(cipher_ctx, NULL, NULL, 0);
-            if (ret) {
-                rc = 1;
-                goto end;
-            }
-        }
+        ret = gcm(tc);
         break;
     default:
         printf("Error: Unsupported AES AEAD mode requested by ACVP server\n");
-        rc = 1;
-        goto end;
+        return 1;
     }
 
-end:
-    /* Cleanup */
-    if (cipher_ctx) EVP_CIPHER_CTX_free(cipher_ctx);
-
-    return rc;
+    return ret;
 }
 
